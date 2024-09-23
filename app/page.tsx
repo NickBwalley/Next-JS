406f98fb8ae5d8e80f612f53ae6476c9a8ff9077
NickBwalley <nickbwalley@gmail.com>
2024-09-23 13:30:27 +0200

    homeImg fill

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,17 @@
-import Link from "next/link";
+import Image from "next/image";
+import homeImg from "@/public/home.jpg";
 export default function Home() {
-  return <div>Homepage</div>;
+  return (
+    <div>
+      Homepage
+      <div className="obsolute -z-10 inset-0">
+        <Image
+          src={homeImg}
+          alt="car factory"
+          fill
+          style={{ objectFit: "cover" }}
+        />
+      </div>
+    </div>
+  );
 }
