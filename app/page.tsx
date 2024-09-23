4a5f6125064faa03b8c02de3b7a4d3ed8e7bd722
NickBwalley <nickbwalley@gmail.com>
2024-09-23 12:53:14 +0200

    common-ui-with-layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,4 @@
 import Link from "next/link";
 export default function Home() {
-  return (
-    <div>
-      <div>
-        <Link href="/performance">Performance</Link>
-        <Link href="/reliability">reliability</Link>
-        <Link href="/scale">scale</Link>
-        <Link href="/">Home</Link>
-      </div>
-    </div>
-  );
+  return <div>Homepage</div>;
 }
